01e4da60c2cd36b887eab3fcf567de42e2d875e5
Asad Meer <meerasads636@gmail.com>
2023-09-05 12:42:31 +0500

    packages done

---
+++ mabweb/src/app/(abouts)/abouts/page.tsx
@@ -0,0 +1,13 @@
+import { relative } from "path"
+
+export default function page(){
+    return(
+        <div className="h-screen bg-gray-800 grid place-content-center  ">
+            <div  className="bg-red-700 text-center w-14 h-11 border ">hallo</div> </div>
+    )
+}
+          // center a div
+// flex justify-center items-center in parent class
+//m-auto in child class and flex use in parent class and grid in parent class anyone 
+//  grid place-content-center in parent class using grid
+// relative in parent class absolute left-1/2 top-1/2 translate x and y to center it  in chlidren class
