3a9522bfc97d31b605173e8f909fbb8520c91e3e
Asad Meer <meerasads636@gmail.com>
2023-09-07 13:07:09 +0500

    login page  input button

--- mabweb/src/app/(login)/login/page.tsx
+++ mabweb/src/app/(login)/login/page.tsx
@@ -33,74 +33,100 @@ export default function page() {
   }
 
   return (
-<main className="h-screen bg-zinc-700 ">
-<div className="flex justify-start  h-16 w-52 ml-14 ">
-  <div className="flex justify-center items-center ">
-    </div> </div>
+    <main className="h-screen bg-zinc-700  ">
 
-    <div className=" flex  justify-center items-center h-full ">
+<title>MAB SOLUTION |Login</title>
 
-      
-      <title>MAB SOLUTION |Login</title>
-      <div className=" w-80 h-3/5 bg-slate-950  rounded-xl p-2  flex flex-col ">
-        <p className=" mt-4 text-center font-bold text-2xl">Login</p>
+ <div className="justify-center  items-center flex">  
+   <div className="justify-center  items-center flex   w-44 h-20 ">
+   <Image
+      src="/logo.png"
+      width={200}
+      height={50}
+      alt="Picture of the author"
+    />
+    
+    
+   </div>
+   </div>
 
 
-        {/* input button */}
-        <div className="mt-4  ml-9 ">
-          <label className="font-mono  text-slate-500 text-lg">Username</label>
 
 
-          <div className="">
 
-            <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
-            ></input>
+      <div className=" flex  justify-center items-center h-full ">
+        
+
+
+        
+        <div className=" w-80 h-3/5 bg-slate-950  rounded-xl p-2  flex flex-col ">
+          <p className=" mt-4 text-center font-bold text-2xl">Login</p>
+
+
+          {/* input button */}
+          <div className="mt-4  ml-9 ">
+            <label className="font-mono  text-slate-500 text-lg">Username</label>
+
+
+            <div className="">
+                      <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i>
+              <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
+            
+              />
+              
+            </div>
           </div>
-        </div>
 
-        <div className="  mx-7  ml-9  mt-2">
-          <label className="font-mono  text-slate-500 text-lg">Password</label>
-          <div className="  ">
-            <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-full border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" id="password" type="password" placeholder="****************" />
+          <div className="  mx-7  ml-9  mt-2">
+           
+            <label className="font-mono  text-slate-500 text-lg">Password</label>
+            
+              <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg></i>
+              <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-full border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" id="password" type="password" placeholder="****************" />
 
 
-
+              
+            
           </div>
 
-        </div>
-
-        {/* forget passworf */}
-        <div className=" flex ml-36 mt-2 font-thin text-base">
-          <a rel="noopener noreferrer" href="#">Forgot Password?</a>
-        </div>
-        {/* button */}
-        <div className="mt-4   flex justify-center items-center">
-          <a href="/home"
-            target="_blank"
-          >
+          {/* forget passworf */}
+          <div className=" flex ml-36 mt-2 font-thin text-base">
+            <a rel="noopener noreferrer" href="#">Forgot Password?</a>
+          </div>
+          {/* button */}
+          <div className="mt-4   flex justify-center items-center">
+            <a href="/home"
+              target="_blank"
+            >
 
 
-            <button className=" bg-purple-400 h-10 w-60 rounded-md text-black " onClick={handleClick}>Sign in
-            </button>
+              <button className=" bg-purple-400 h-10 w-60 rounded-md text-black " onClick={handleClick}>Sign in
+              </button>
+            </a>
             <ToastContainer />
-          </a>
-        </div>
+          </div>
 
 
 
-        <div className="flex justify-center items-center font-thin text-base mt-4">
-          Don't have a account? <button className="text-lg font-normal ">Sign up</button>
+          <div className="flex justify-center items-center font-thin text-base mt-4">
+
+            Don't have a account? <button className="text-lg font-normal " >
+              <a href="/signup"
+                target="_blank"> Sign up
+              </a>
+            </button>
+
+          </div>
+
+
         </div>
 
 
       </div>
 
 
-    </div>
-  
-  
 
-</main>
+    </main>
 
 
 
---
+++ mabweb/src/app/(signup)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){
+    return(
+        <div  className="bg-zinc-700 h-screen flex justify-center items-center">
+          <div className=" w-50 h-50 flex justify-center items-center text-7xl font-extrabold">signup</div>
+            </div>
+    )
+}
