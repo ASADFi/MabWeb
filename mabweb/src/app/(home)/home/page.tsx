325c9e12d9265dd09be50dcfcba352a7064c8f60
Asad Meer <meerasads636@gmail.com>
2023-09-08 15:48:30 +0500

    home page UI

--- mabweb/src/app/(home)/home/page.tsx
+++ mabweb/src/app/(home)/home/page.tsx
@@ -1,7 +1,135 @@
-export default function page(){
-    return(
-        <div className="h-screen flex  justify-center items-center bg-zinc-700">
-            <div className="text-8xl font-bold text-amber-400 hover:text-red-500 hover:bg-black hover:rounded-2xl hover:-1/5">MAB SOLUTION HOME PAGE
-            </div> </div>
+import Image from "next/image"
+export default function page() {
+    return (
+        <main className="h-screen w-full bg-zinc-700 overflow-hidden  ">
+            <section className="   w-full h-20 flex justify-between  ">
+
+                <div className="ml-9   mt-5 ">
+                    <a href="/home">
+                        <Image
+                            src="/logo.png"
+                            width={200}
+                            height={50}
+                            alt="Picture of the author"
+
+                        />
+                    </a>
+                </div>
+
+
+
+                <p className=" mr-9 mt-5    ">
+                    <a href="/login" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-purple-400 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                        <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Log Out</span>
+                    </a>
+
+                </p>
+
+
+            </section>
+
+
+
+
+
+
+
+
+
+            <section className="  flex justify-center  items-center">
+                <div className="w-2/3 h-full bg-slate-950  rounded-xl  mt-9   shadow-[0_0_10px_purple] ">
+                <h1 className="flex  justify-center    text-3xl font-mono ">
+                    WELCOME MAB!
+                </h1>
+
+                <div className="flex  justify-center  items-center flex-col bg-red-600 gap-20 mt-5 ">
+                 <div className=" w-44 ">
+                    
+                    
+                    
+                    <label className="font-mono  text-slate-500 text-lg">Username</label>
+
+
+<div className="">
+  {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
+  <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
+
+  />
+
+</div></div>
+                    </div>
+                <div className="flex  justify-center  items-center flex-col bg-red-600 gap-20 mt-5 ">
+                 <div className=" w-44 "><label className="font-mono  text-slate-500 text-lg">Username</label>
+
+
+<div className="">
+  {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
+  <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
+
+  />
+
+</div></div>
+                    </div>
+                <div className="flex  justify-center  items-center flex-col bg-red-600 gap-20 mt-5 ">
+                 <div className=" w-44 "><label className="font-mono  text-slate-500 text-lg">Username</label>
+
+
+<div className="">
+  {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
+  <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
+
+  />
+
+</div></div>
+                    </div>
+                <div className="flex  justify-center  items-center flex-col  gap-20 mt-5 ">
+                 <div className=" w-44 "><label className="font-mono  text-slate-500 text-lg">Username</label>
+
+
+<div className="">
+  {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
+  <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
+
+  />
+
+</div></div>
+                    </div>
+                
+                </div>
+
+
+            </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </main>
     )
 }
--- mabweb/src/app/(login)/login/page.tsx
+++ mabweb/src/app/(login)/login/page.tsx
@@ -34,35 +34,35 @@ export default function page() {
 
   return (
     <main className="h-screen bg-zinc-700  ">
-<head>
-<title>MAB SOLUTION |Login</title>
-  <link rel="icon" type="image/png" href="/logo.png"/>
-</head>
+      <head>
+        <title>MAB SOLUTION |Login</title>
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </head>
 
 
 
- <div className="justify-center  items-center flex">  
-   <div className="justify-center  items-center flex   w-44 h-20 ">
-   <Image
-      src="/logo.png"
-      width={200}
-      height={50}
-      alt="Picture of the author"
-    />
-    
-    
-   </div>
-   </div>
+      <div className="justify-center  items-center flex">
+        <div className="justify-center  items-center flex   w-44 h-20 ">
+          <Image
+            src="/logo.png"
+            width={200}
+            height={50}
+            alt="Picture of the author"
+          />
+
+
+        </div>
+      </div>
 
 
 
 
 
       <div className=" flex  justify-center  ">
-        
 
 
-        
+
+
         <div className=" w-80 h-96 bg-slate-950  rounded-xl  mt-9  ">
           <p className=" mt-4 text-center font-bold text-2xl">Login</p>
 
@@ -73,25 +73,25 @@ export default function page() {
 
 
             <div className="">
-                      {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
+              {/* <i className=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i> */}
               <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" placeholder="name..." type="text" name="name"
-            
+
               />
-              
+
             </div>
           </div>
 
           <div className="  mx-7  ml-9  mt-2">
-           
+
             <label className="font-mono  text-slate-500 text-lg">Password</label>
-           
-              {/* <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg></i> */}
-              <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" id="password" type="password" placeholder="****************" />
-              
+
+            {/* <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg></i> */}
+            <input className="placeholder:italic mt-1 placeholder:text-slate-400 block bg-slate-950 h-10 w-60 border border-slate-400 rounded-lg py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-purple-400 focus:ring-purple-400 focus:ring-1 sm:text-sm" id="password" type="password" placeholder="****************" />
 
 
-              
-            
+
+
+
           </div>
 
           {/* forget passworf */}
@@ -101,7 +101,7 @@ export default function page() {
           {/* button */}
           <div className="mt-4   flex justify-center items-center">
             <a href="/home"
-            
+
             >
 
 
@@ -117,7 +117,7 @@ export default function page() {
 
             Don't have a account? <button className="text-lg font-normal " >
               <a href="/signup"
-                > Sign up
+              > Sign up
               </a>
             </button>
 
