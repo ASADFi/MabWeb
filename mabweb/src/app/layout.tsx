32109e86285a0aecb8d3e0c16b29f405d0bf69eb
Asad Meer <meerasads636@gmail.com>
2023-09-12 11:06:11 +0500

    nav bar

--- mabweb/src/app/layout.tsx
+++ mabweb/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
 
     <html lang="en">
+      
       <body className={inter.className}>{children}</body>
       <link rel="icon" type="image/png" href="/logo.png"/>
     </html>
